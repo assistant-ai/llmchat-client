6d6a42fe3bded44c40b0d25bd3f35b87d4540760
Nikita <5195769+nikkrichko@users.noreply.github.com>
2023-07-11 19:38:52 -0700

    move gpt clients models utils to llm client. Nothing affects. limit max tokens for some models

---
+++ gpt/gpt_utils.go
@@ -0,0 +1,45 @@
+package gpt
+
+import "strings"
+
+func GetLlmClientGptModels() map[string]*GPTModel {
+	Models := make(map[string]*GPTModel)
+	Models["gpt3Turbo"] = ModelGPT3Turbo
+	Models["gpt3TurboBig"] = ModelGPT3TurboBig
+	Models["gpt4"] = ModelGPT4
+	Models["gpt4Big"] = ModelGPT4Big
+	return Models
+}
+
+func GetListOfModels() []string {
+	modelsMap := GetLlmClientGptModels()
+	keys := make([]string, 0, len(modelsMap))
+	for key := range modelsMap {
+		keys = append(keys, key)
+	}
+	return keys
+}
+
+func IsModelGPTValid(modelName string) bool {
+	if notInList(modelName, GetListOfModels()) {
+		return false
+	}
+	if !isModelGpt(modelName) {
+		return false
+	}
+	return true
+}
+
+func notInList(target string, list []string) bool {
+	for _, item := range list {
+		if item == target {
+			return false
+		}
+	}
+	return true
+}
+
+func isModelGpt(modelName string) bool {
+	contains := strings.Contains(strings.ToLower(modelName), "gpt")
+	return contains
+}
